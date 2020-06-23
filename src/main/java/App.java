7fbaedc262f453e3ec2a302216e4f613e9212a75
Stepan Damrin <sr.stepan.damrin@gmail.com>
2020-06-24 00:32:43 +0300

    Task Done

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,11 +2,20 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class App {
-    public static void main(String[] args) {
-        ApplicationContext applicationContext =
-                new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
-    }
+
+  public static void main(String[] args) {
+    ApplicationContext applicationContext =
+        new AnnotationConfigApplicationContext(AppConfig.class);
+    HelloWorld beanHWFirst =
+        (HelloWorld) applicationContext.getBean("helloworld");
+    HelloWorld beanHWSecond =
+        (HelloWorld) applicationContext.getBean("helloworld");
+    Cat beanCatirst =
+        (Cat) applicationContext.getBean("cat");
+    Cat beanCatSecond =
+        (Cat) applicationContext.getBean("cat");
+    System.out.println(beanHWFirst.equals(beanHWSecond));
+    System.out.println(beanCatirst.equals(beanCatSecond));
+
+  }
 }
